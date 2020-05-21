566df8afb906d73a6a5f2d312c6ca98895e9f1a4
sirius97 <sirimukund@gmail.com>
2020-05-21 18:09:36 +0530

    Updates

---
+++ precache-manifest.0eb26c39a0f4a9e3c89317f92984299c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9987de5316b5ac011206f8daf5f97c68",
+    "url": "/Covid-19-Live-1/index.html"
+  },
+  {
+    "revision": "a09eae72921d9abd5d0f",
+    "url": "/Covid-19-Live-1/static/css/main.34399bc0.chunk.css"
+  },
+  {
+    "revision": "e06f63586c4417961901",
+    "url": "/Covid-19-Live-1/static/js/2.9e9aaa63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid-19-Live-1/static/js/2.9e9aaa63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09eae72921d9abd5d0f",
+    "url": "/Covid-19-Live-1/static/js/main.ebcd7c3c.chunk.js"
+  },
+  {
+    "revision": "79362a732f4ef114292e",
+    "url": "/Covid-19-Live-1/static/js/runtime-main.eb50f26a.js"
+  }
+]);
